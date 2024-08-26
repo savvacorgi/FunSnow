c1a281f2c48b7fc7c7ac6001c511c20020411c9e
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-26 22:48:56 +0300

    Update SphereListener.java

--- src/main/java/com/svo/snowp/listeners/SphereListener.java
+++ src/main/java/com/svo/snowp/listeners/SphereListener.java
@@ -1,30 +1,20 @@
-package com.svo.snowp.listeners;
-
-import com.svo.snowp.Snowp;
-import com.svo.snowp.utils.SphereUtils;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.block.Block;
+import org.bukkit.event.block.Action;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
 
 public class SphereListener implements Listener {
-
-    private final Snowp plugin;
-
-    public SphereListener(Snowp plugin) {
-        this.plugin = plugin;
-    }
-
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        Block block = event.getClickedBlock();
-        Player player = event.getPlayer();
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getClickedBlock() == null) return;
 
-        if (block != null && block.getType() == Material.PLAYER_HEAD) {
-            SphereUtils.giveRandomSphere(player);
-            block.setType(Material.AIR); // Удаляем кейс после открытия
-        }
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        SphereUtils.openGiftBox(player, item);
     }
 }
