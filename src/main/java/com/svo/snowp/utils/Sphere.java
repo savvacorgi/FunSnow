764a10465811b71f1ec26f14dc021b53251feb26
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-26 23:26:44 +0300

    Create Sphere.java

---
+++ src/main/java/com/svo/snowp/utils/Sphere.java
@@ -0,0 +1,23 @@
+package com.svo.snowp.utils;
+
+public class Sphere {
+    private String name;
+    private String headName;
+    private String id;
+    private double damage;
+    private double health;
+    private double speed;
+    private String description;
+
+    public Sphere(String name, String headName, String id, double damage, double health, double speed, String description) {
+        this.name = name;
+        this.headName = headName;
+        this.id = id;
+        this.damage = damage;
+        this.health = health;
+        this.speed = speed;
+        this.description = description;
+    }
+
+    // Геттеры и сеттеры
+}
