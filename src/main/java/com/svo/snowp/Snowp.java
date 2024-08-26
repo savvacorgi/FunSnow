568492d02652a27900a1cb58e11d14d3686cb3a7
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-26 22:49:13 +0300

    Update Snowp.java

--- src/main/java/com/svo/snowp/Snowp.java
+++ src/main/java/com/svo/snowp/Snowp.java
@@ -1,77 +1,27 @@
-package com.svo.snowp;
-
-import com.svo.snowp.listeners.BlockPlaceListener;
-import com.svo.snowp.listeners.SphereListener;
-import com.svo.snowp.utils.SphereUtils;
-import com.svo.snowp.utils.TelegramNotifier;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.NamespacedKey;
 
 public class Snowp extends JavaPlugin {
-
-    private TelegramNotifier telegramNotifier;
-    private SphereUtils sphereUtils;
-
     @Override
     public void onEnable() {
-        // Регистрация слушателей событий
-        Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new SphereListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new SphereListener(), this);
 
-        // Инициализация утилит
-        sphereUtils = new SphereUtils(this);
-
-        // Создание рецепта
-        createCustomSnowBlockRecipe();
-
-        // Инициализация и использование TelegramNotifier
-        this.saveDefaultConfig(); // Убедитесь, что конфиг файл создан
-        telegramNotifier = new TelegramNotifier(this);
-        telegramNotifier.sendNotification("Server is now online!");
-
-        // Запуск ивента
-        startEvent();
-
-        getLogger().info("Snowp plugin enabled.");
+        // Create and register recipes if needed
+        NamespacedKey key = new NamespacedKey(this, "custom_snow_block");
+        ItemStack customSnowBlock = SphereUtils.createSphereItem(new Sphere("Custom Snow Block", "Custom Snow Block Head", "100683", 0, 0, 0, ""));
+        ShapedRecipe recipe = new ShapedRecipe(key, customSnowBlock);
+        recipe.shape("XXX", "XYX", "XXX");
+        recipe.setIngredient('X', Material.SNOW_BLOCK);
+        recipe.setIngredient('Y', Material.DIAMOND);
+        Bukkit.addRecipe(recipe);
     }
 
     @Override
     public void onDisable() {
-        if (telegramNotifier != null) {
-            telegramNotifier.sendNotification("Server is shutting down.");
-        }
-        getLogger().info("Snowp plugin disabled.");
-    }
-
-    private void createCustomSnowBlockRecipe() {
-        ItemStack customSnowBlock = SphereUtils.createCustomSnowBlock();
-        NamespacedKey key = new NamespacedKey(this, "custom_snow_block");
-        ShapedRecipe recipe = new ShapedRecipe(key, customSnowBlock);
-        recipe.shape("SSS", "SCS", "SSS");
-        recipe.setIngredient('S', Material.SNOWBALL);
-        recipe.setIngredient('C', Material.DIAMOND);
-        Bukkit.addRecipe(recipe);
-    }
-
-    private void startEvent() {
-        // Запускаем ивент сразу после включения плагина
-        sphereUtils.spawnGiftBoxes(Bukkit.getWorlds().get(0)); // Замените на нужный мир
-
-        // Запускаем повторяющийся ивент каждые 15 минут
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                sphereUtils.spawnGiftBoxes(Bukkit.getWorlds().get(0)); // Замените на нужный мир
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        sphereUtils.removeGiftBoxes(Bukkit.getWorlds().get(0)); // Замените на нужный мир
-                    }
-                }.runTaskLater(Snowp.this, 3600L); // 3 минуты позже
-            }
-        }.runTaskTimer(this, 0L, 18000L); // Каждые 15 минут (20 тик = 1 секунда)
+        // Cleanup code if needed
     }
 }
