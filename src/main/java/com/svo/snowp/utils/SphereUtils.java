061e45f9a4bd48f7553ea2c3294c6f26470952ca
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-26 15:01:55 +0300

    Update SphereUtils.java

--- src/main/java/com/svo/snowp/utils/SphereUtils.java
+++ src/main/java/com/svo/snowp/utils/SphereUtils.java
@@ -5,18 +5,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Collections;
 import java.util.Random;
 
 public class SphereUtils {
 
+    // Применение эффектов сферы к игроку
     public static void applyCustomEffects(Player player, ItemStack item) {
         SkullMeta meta = (SkullMeta) item.getItemMeta();
         if (meta == null || !meta.hasDisplayName()) return;
@@ -27,14 +29,14 @@ public class SphereUtils {
 
         switch (displayName.toLowerCase()) {
             case "ez sphere":
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, 5.0);
-                modifyAttribute(player, Attribute.GENERIC_MAX_HEALTH, -3.5);
-                modifyAttribute(player, Attribute.GENERIC_MOVEMENT_SPEED, 0.1);
-                modifyAttribute(player, Attribute.GENERIC_ARMOR, 2.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, 5.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH, -3.5);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED, 0.1);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ARMOR, 2.0);
                 break;
             case "turtle sphere":
-                modifyAttribute(player, Attribute.GENERIC_MOVEMENT_SPEED, -0.25);
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, 4.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED, -0.25);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, 4.0);
                 new BukkitRunnable() {
                     @Override
                     public void run() {
@@ -47,24 +49,24 @@ public class SphereUtils {
                 }.runTaskTimer(JavaPlugin.getPlugin(Snowp.class), 0L, 40L);
                 break;
             case "miner sphere":
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_SPEED, 2.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_SPEED, 2.0);
                 player.setFireTicks(0);
-                modifyAttribute(player, Attribute.GENERIC_MAX_HEALTH, -2.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH, -2.0);
                 break;
             case "tank sphere":
-                modifyAttribute(player, Attribute.GENERIC_ARMOR, 4.0);
-                modifyAttribute(player, Attribute.GENERIC_MAX_HEALTH, 4.0);
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, -4.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ARMOR, 4.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH, 4.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, -4.0);
                 break;
             case "dumb sphere":
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, 6.0);
-                modifyAttribute(player, Attribute.GENERIC_MAX_HEALTH, -2.0);
-                modifyAttribute(player, Attribute.GENERIC_ARMOR, -3.0);
-                modifyAttribute(player, Attribute.GENERIC_MOVEMENT_SPEED, -0.1);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, 6.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH, -2.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ARMOR, -3.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED, -0.1);
                 break;
             case "tap tap monster sphere":
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_SPEED, 3.0);
-                modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, -5.5);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_SPEED, 3.0);
+                modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, -5.5);
                 break;
             default:
                 break;
@@ -72,15 +74,15 @@ public class SphereUtils {
     }
 
     public static void resetPlayerAttributes(Player player) {
-        modifyAttribute(player, Attribute.GENERIC_ATTACK_DAMAGE, 0.0);
-        modifyAttribute(player, Attribute.GENERIC_MAX_HEALTH, 0.0);
-        modifyAttribute(player, Attribute.GENERIC_MOVEMENT_SPEED, 0.0);
-        modifyAttribute(player, Attribute.GENERIC_ARMOR, 0.0);
-        modifyAttribute(player, Attribute.GENERIC_ATTACK_SPEED, 0.0);
+        modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE, 0.0);
+        modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH, 0.0);
+        modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED, 0.0);
+        modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ARMOR, 0.0);
+        modifyAttribute(player, org.bukkit.attribute.Attribute.GENERIC_ATTACK_SPEED, 0.0);
     }
 
-    private static void modifyAttribute(Player player, Attribute attribute, double value) {
-        var attributeInstance = player.getAttribute(attribute);
+    private static void modifyAttribute(Player player, org.bukkit.attribute.Attribute attribute, double value) {
+        org.bukkit.attribute.AttributeInstance attributeInstance = player.getAttribute(attribute);
         if (attributeInstance != null) {
             attributeInstance.setBaseValue(attributeInstance.getBaseValue() + value);
         }
@@ -104,50 +106,46 @@ public class SphereUtils {
     public static void spawnRandomCase(World world) {
         Random random = new Random();
         if (random.nextInt(100) < 8) { // 8% шанс спавна в чанке
-            int x = random.nextInt(16);
-            int z = random.nextInt(16);
+            int x = random.nextInt(1000) - 500;
+            int z = random.nextInt(1000) - 500;
             int y = world.getHighestBlockYAt(x, z);
             Block block = world.getBlockAt(x, y, z);
 
             if (block.getType() == Material.GRASS_BLOCK || block.getType() == Material.SNOW_BLOCK) {
                 block.setType(Material.PLAYER_HEAD);
-                SkullMeta meta = (SkullMeta) block.getState().getBlock().getState();
-                meta.setOwningPlayer(Bukkit.getOfflinePlayer("savvacorgi"));
-                block.getState().update();
+                SkullMeta meta = (SkullMeta) block.getState().getData();
+                meta.setDisplayName(ChatColor.GREEN + "Present");
+                meta.setOwner("MHF_Present"); // Здесь можно указать URL или owner для создания кастомного скина
+                block.getState().setData(meta);
             }
         }
     }
 
-    public static ItemStack createSphereItem(String name) {
-        ItemStack sphere = new ItemStack(Material.PLAYER_HEAD);
-        SkullMeta meta = (SkullMeta) sphere.getItemMeta();
-
+    // Создание предмета для события
+    public static ItemStack createHappyNewYearItem() {
+        ItemStack item = new ItemStack(Material.FIREWORK_ROCKET);
+        ItemMeta meta = item.getItemMeta();
         if (meta != null) {
-            meta.setDisplayName(ChatColor.GOLD + name);
-            meta.setOwningPlayer(Bukkit.getOfflinePlayer("savvacorgi"));
-            sphere.setItemMeta(meta);
+            meta.setDisplayName(ChatColor.GOLD + "Happy New Year Firework");
+            meta.addEnchant(org.bukkit.enchantments.Enchantment.LUCK, 1, true);
+            item.setItemMeta(meta);
         }
-
-        return sphere;
+        return item;
     }
 
-    public static void giveRandomSphere(Player player) {
-        Random random = new Random();
-        int roll = random.nextInt(100);
+    // Запуск события нового года
+    public static void startNewYearEvent(Player player, Snowp plugin) {
+        plugin.getServer().broadcastMessage(ChatColor.GREEN + "Новый год пришел, у вас есть 10 минут!");
+        changeBiomeToTaiga(player);
 
-        ItemStack sphere;
-
-        if (roll < 50) { // 50% шанс
-            sphere = createSphereItem("EZ Sphere");
-        } else if (roll < 80) { // 30% шанс
-            sphere = createSphereItem("Turtle Sphere");
-        } else if (roll < 95) { // 15% шанс
-            sphere = createSphereItem("Miner Sphere");
-        } else { // 5% шанс
-            sphere = createSphereItem("Tank Sphere");
-        }
-
-        player.getInventory().addItem(sphere);
-        player.sendMessage(ChatColor.GREEN + "You received: " + sphere.getItemMeta().getDisplayName());
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // Спавн кейсов в радиусе 500 блоков от игрока
+                for (int i = 0; i < 3; i++) {
+                    spawnRandomCase(player.getWorld());
+                }
+            }
+        }.runTaskLater(JavaPlugin.getPlugin(Snowp.class), 20L * 10 * 60); // 10 минут
     }
 }
