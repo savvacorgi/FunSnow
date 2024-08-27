adb1fb13180054b932ac48ca1d53fa704336ef5d
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 13:30:00 +0300

    Update Snowp.java

--- src/main/java/com/svo/snowp/Snowp.java
+++ src/main/java/com/svo/snowp/Snowp.java
@@ -13,14 +13,17 @@ public class Snowp extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Инициализация утилит и менеджера событий
         sphereUtils = new SphereUtils();
         eventManager = new EventManager(this);
 
+        // Регистрация слушателей событий
         Bukkit.getPluginManager().registerEvents(new SphereListener(eventManager, sphereUtils), this);
 
-        // Регистрация Water TNT
+        // Регистрация Water TNT и его рецепта
         WaterTNT waterTNT = new WaterTNT(this);
         Bukkit.getPluginManager().registerEvents(waterTNT, this);
+        waterTNT.addRecipe();
 
         // Автоматический запуск ивента при старте сервера
         eventManager.startEvent();
