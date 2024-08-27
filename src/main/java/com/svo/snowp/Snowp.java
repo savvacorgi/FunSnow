7f3452f161fa543f7266762fc9448b5cdec63ae2
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 14:29:27 +0300

    Update Snowp.java

--- src/main/java/com/svo/snowp/Snowp.java
+++ src/main/java/com/svo/snowp/Snowp.java
@@ -2,9 +2,9 @@ package com.svo.snowp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.svo.snowp.items.CustomWaterTNT;
 import com.svo.snowp.listeners.SphereListener;
 import com.svo.snowp.utils.SphereUtils;
-import com.svo.snowp.items.WaterTNT;
 
 public class Snowp extends JavaPlugin {
 
@@ -15,27 +15,19 @@ public class Snowp extends JavaPlugin {
     public void onEnable() {
         // Инициализация утилит и менеджера событий
         sphereUtils = new SphereUtils();
-        eventManager = new EventManager(this, sphereUtils); // Передаем sphereUtils в EventManager
+        eventManager = new EventManager(this);
 
         // Регистрация слушателей событий
-        Bukkit.getPluginManager().registerEvents(new SphereListener(sphereUtils), this); // Передаем только sphereUtils в SphereListener
+        Bukkit.getPluginManager().registerEvents(new SphereListener(sphereUtils), this);
 
         // Регистрация Water TNT и его рецепта
-        WaterTNT waterTNT = new WaterTNT(this);
-        Bukkit.getPluginManager().registerEvents(waterTNT, this);
-        waterTNT.addRecipe();
-
-        // Автоматический запуск ивента при старте сервера
-        if (!eventManager.isEventActive()) {
-            eventManager.startEvent();
-        }
+        CustomWaterTNT customWaterTNT = new CustomWaterTNT(this);
+        Bukkit.getPluginManager().registerEvents(customWaterTNT, this);
+        customWaterTNT.addRecipe();
     }
 
     @Override
     public void onDisable() {
         // Логика при отключении плагина
-        if (eventManager.isEventActive()) {
-            eventManager.endEvent(); // Завершаем ивент при отключении
-        }
     }
 }
