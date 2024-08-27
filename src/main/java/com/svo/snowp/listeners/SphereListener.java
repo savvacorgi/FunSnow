d08a03c9935d04a3eb776fcb95d608fcca9a4988
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 14:20:20 +0300

    Update SphereListener.java

--- src/main/java/com/svo/snowp/listeners/SphereListener.java
+++ src/main/java/com/svo/snowp/listeners/SphereListener.java
@@ -2,14 +2,15 @@ package com.svo.snowp.listeners;
 
 import com.svo.snowp.utils.Sphere;
 import com.svo.snowp.utils.SphereUtils;
-import org.bukkit.entity.Player;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.entity.Player;
+
+import java.util.Random;
 
 public class SphereListener implements Listener {
 
@@ -26,39 +27,16 @@ public class SphereListener implements Listener {
             ItemMeta meta = item.getItemMeta();
             if (meta != null && meta.hasDisplayName()) {
                 String displayName = meta.getDisplayName();
-                Sphere sphere = sphereUtils.getRandomSphere(); // Получение сферы для обработки эффекта
 
-                // Найти сферу по имени
-                for (Sphere s : sphereUtils.getSpheres()) {
-                    if (displayName.equals(s.getDisplayName())) {
-                        // Применение эффектов сферы
-                        Player player = event.getPlayer();
-                        
-                        if (s.getAttackBoost() > 0) {
-                            player.sendMessage("Увеличение урона на " + s.getAttackBoost());
-                            // Ваш код для увеличения урона игрока
-                        }
-                        if (s.getHealthReduction() < 0) {
-                            player.sendMessage("Уменьшение здоровья на " + Math.abs(s.getHealthReduction()));
-                            // Ваш код для уменьшения здоровья игрока
-                            player.setHealth(Math.max(player.getHealth() + s.getHealthReduction(), 0));
-                        }
-                        if (s.getMovementSpeedBoost() > 0) {
-                            player.sendMessage("Увеличение скорости передвижения на " + s.getMovementSpeedBoost());
-                            // Ваш код для увеличения скорости передвижения
-                            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, (int) s.getMovementSpeedBoost() - 1));
-                        }
-                        if (s.getDefenseBoost() > 0) {
-                            player.sendMessage("Увеличение защиты на " + s.getDefenseBoost());
-                            // Ваш код для увеличения защиты игрока
-                            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 600, (int) s.getDefenseBoost() - 1));
-                        }
+                // Убедитесь, что это подарок
+                if (displayName.equals("Подарок")) {
+                    // Удаляем голову и добавляем случайную сферу
+                    event.getPlayer().getInventory().removeItem(item);
 
-                        // Удаление сферы из инвентаря
-                        player.getInventory().removeItem(item);
-                        player.sendMessage("Эффект сферы " + displayName + " активирован!");
-                        break; // Прекратить обработку после нахождения сферы
-                    }
+                    Sphere randomSphere = sphereUtils.getRandomSphere(); // Получение случайной сферы
+                    ItemStack sphereItem = sphereUtils.createSphereItem(randomSphere);
+                    event.getPlayer().getInventory().addItem(sphereItem);
+                    event.getPlayer().sendMessage("Вы нашли подарок и получили: " + randomSphere.getName() + "!");
                 }
             }
         }
