7172259a3f857c8be8004010889adb86fa3a3d60
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 14:19:27 +0300

    Update SphereUtils.java

--- src/main/java/com/svo/snowp/utils/SphereUtils.java
+++ src/main/java/com/svo/snowp/utils/SphereUtils.java
@@ -1,13 +1,8 @@
 package com.svo.snowp.utils;
 
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,53 +13,31 @@ public class SphereUtils {
 
     public SphereUtils() {
         // Инициализация списка сфер
-        spheres.add(new Sphere("Sphere Miner", "Miner Head", 1.0, 0.0, 0.0, 0.0));
-        spheres.add(new Sphere("Sphere Diver", "Diver Head", 0.0, 1.0, 0.0, 0.0));
-        spheres.add(new Sphere("Sphere Builder", "Builder Head", 0.0, 0.0, 1.0, 0.0));
-        spheres.add(new Sphere("Sphere Explorer", "Explorer Head", 0.0, 0.0, 1.0, 0.0));
-        spheres.add(new Sphere("Sphere Warrior", "Warrior Head", 2.0, 0.0, 0.0, 1.0));
-        spheres.add(new Sphere("Sphere Archer", "Archer Head", 0.0, 0.0, 1.0, 0.0));
-        spheres.add(new Sphere("Sphere Mage", "Mage Head", 0.0, 0.0, 0.0, 1.0));
-        spheres.add(new Sphere("Sphere Knight", "Knight Head", 0.0, 0.0, 0.0, 1.0));
-        spheres.add(new Sphere("Sphere of Agility", "Agility Head", 4.0, -3.0, 10.0, 2.0));
+        spheres.add(new Sphere("Sphere Miner", "Miner Head", "100683", 0.0, 0.0, 1.0, "Increased mining speed"));
+        spheres.add(new Sphere("Sphere Diver", "Diver Head", "100683", 0.0, 0.0, 1.0, "Increased underwater speed"));
+        spheres.add(new Sphere("Sphere Builder", "Builder Head", "100683", 0.0, 0.0, 1.0, "Increased building speed"));
+        spheres.add(new Sphere("Sphere Explorer", "Explorer Head", "100683", 0.0, 0.0, 1.0, "Increased exploration speed"));
+        spheres.add(new Sphere("Sphere Warrior", "Warrior Head", "100683", 1.0, 0.0, 1.0, "Increased combat ability"));
+        spheres.add(new Sphere("Sphere Archer", "Archer Head", "100683", 0.0, 0.0, 1.0, "Increased archery skills"));
+        spheres.add(new Sphere("Sphere Mage", "Mage Head", "100683", 0.0, 0.0, 1.0, "Increased magical abilities"));
+        spheres.add(new Sphere("Sphere Knight", "Knight Head", "100683", 0.0, 0.0, 1.0, "Balanced combat skills"));
+        spheres.add(new Sphere("Sphere of Agility", "Agility Head", "100684", 4.0, -3.0, 2.0, "Increase damage by 4, decrease HP by 3, increase movement speed by 10, increase defense by 2"));
+        // Добавьте больше сфер по необходимости
     }
 
+    // Метод для создания предмета сферы
     public ItemStack createSphereItem(Sphere sphere) {
         ItemStack item = new ItemStack(Material.PLAYER_HEAD);
         ItemMeta meta = item.getItemMeta();
-        if (meta != null) {
-            meta.setDisplayName(sphere.getDisplayName());
-            meta.setLore(List.of("§4Attack Boost: " + sphere.getAttackBoost(),
-                    "§4Health Reduction: " + sphere.getHealthReduction(),
-                    "§4Movement Speed Boost: " + sphere.getMovementSpeedBoost(),
-                    "§4Defense Boost: " + sphere.getDefenseBoost()));
-            item.setItemMeta(meta);
-        }
+        meta.setDisplayName(sphere.getName());
+        meta.setLore(List.of(sphere.getDescription()));
+        item.setItemMeta(meta);
         return item;
     }
 
-    public void openGiftBox(Player player, ItemStack item, Plugin plugin) {
-        Sphere randomSphere = spheres.get(new Random().nextInt(spheres.size()));
-        ItemStack sphereItem = createSphereItem(randomSphere);
-        player.getInventory().addItem(sphereItem);
-        player.sendMessage("Вы открыли подарок и получили: " + randomSphere.getDisplayName());
-    }
-
-    public void spawnGiftsInWorld(World world) {
-        Random random = new Random();
-        for (int i = 0; i < 6; i++) {
-            int x = random.nextInt(9900) + 100;
-            int z = random.nextInt(9900) + 100;
-            int y = world.getHighestBlockYAt(x, z);
-            Block block = world.getBlockAt(x, y, z);
-            block.setType(Material.PLAYER_HEAD);
-            ItemStack gift = createSphereItem(spheres.get(random.nextInt(spheres.size())));
-            block.getWorld().dropItemNaturally(block.getLocation(), gift);
-            world.getPlayers().forEach(player -> player.sendMessage("Подарок спавнится на координатах: " + x + ", " + y + ", " + z));
-        }
-    }
-
+    // Получение случайной сферы
     public Sphere getRandomSphere() {
-        return spheres.get(new Random().nextInt(spheres.size()));
+        Random random = new Random();
+        return spheres.get(random.nextInt(spheres.size()));
     }
 }
