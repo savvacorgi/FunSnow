7b1bc9850acb77cbd4e15ae0733ff1fc192cd4fb
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 12:17:45 +0300

    Update Snowp.java

--- src/main/java/com/svo/snowp/Snowp.java
+++ src/main/java/com/svo/snowp/Snowp.java
@@ -1,9 +1,13 @@
 package com.svo.snowp;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.svo.snowp.listeners.SphereListener;
 import com.svo.snowp.utils.SphereUtils;
+import com.svo.snowp.items.WaterTNT;
 
 public class Snowp extends JavaPlugin {
 
@@ -16,6 +20,18 @@ public class Snowp extends JavaPlugin {
         eventManager = new EventManager(this);
 
         Bukkit.getPluginManager().registerEvents(new SphereListener(eventManager, sphereUtils), this);
+
+        // Регистрация Water TNT
+        WaterTNT waterTNT = new WaterTNT(this);
+        Bukkit.getPluginManager().registerEvents(waterTNT, this);
+
+        // Крафт для Water TNT
+        ItemStack waterTNTItem = waterTNT.createWaterTNT();
+        ShapedRecipe recipe = new ShapedRecipe(waterTNTItem);
+        recipe.shape(" S ", "STT", " S ");
+        recipe.setIngredient('S', Material.SAND);
+        recipe.setIngredient('T', Material.TNT);
+        Bukkit.addRecipe(recipe);
 
         // Автоматический запуск ивента при старте сервера
         eventManager.startEvent();
