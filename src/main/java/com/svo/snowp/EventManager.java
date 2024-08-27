517652d19c30ef88ce3b404f7f0555e85b434cf8
Savvacorgi <143608608+savvacorgi@users.noreply.github.com>
2024-08-27 12:20:14 +0300

    Update EventManager.java

--- src/main/java/com/svo/snowp/EventManager.java
+++ src/main/java/com/svo/snowp/EventManager.java
@@ -9,20 +9,24 @@ public class EventManager {
     private final Plugin plugin;
     private final int EVENT_COOLDOWN = 6000; // Кулдаун в тиках (5 минут)
     private final int EVENT_DURATION = 6000; // Длительность ивента в тиках (5 минут)
+    private final GiftManager giftManager;
 
     public EventManager(Plugin plugin) {
         this.plugin = plugin;
+        this.giftManager = new GiftManager(plugin);
     }
 
     public void startEvent() {
-        Bukkit.broadcastMessage("Ивент начался!");
+        Bukkit.broadcastMessage("Ивент начался! Подарки разбросаны по миру, ищите!");
 
-        // Логика ивента
+        // Спавн 5 подарков в случайных местах
+        giftManager.spawnGifts(5);
+
         new BukkitRunnable() {
             @Override
             public void run() {
-                Bukkit.broadcastMessage("Ивент завершился!");
-                scheduleNextEvent();  // Планируем запуск следующего ивента после кулдауна
+                Bukkit.broadcastMessage("Ивент завершился! Все подарки собраны или исчезли.");
+                scheduleNextEvent(); // Планируем запуск следующего ивента после кулдауна
             }
         }.runTaskLater(plugin, EVENT_DURATION);
     }
@@ -31,7 +35,7 @@ public class EventManager {
         new BukkitRunnable() {
             @Override
             public void run() {
-                startEvent();  // Запускаем следующий ивент после кулдауна
+                startEvent(); // Запускаем следующий ивент после кулдауна
             }
         }.runTaskLater(plugin, EVENT_COOLDOWN);
     }
